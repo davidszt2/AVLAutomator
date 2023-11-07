dd6bbe064ae584e90ef6f73c4638029a811a1f5f
David Moeller Sztajnbok <david.sztajnbok@gmail.com>
2023-11-07 01:54:17 -0800

    Working AVL functions

--- AVL.py
+++ AVL.py
@@ -2,45 +2,104 @@
 AVL automation functions for running AVL subprocess through Python
 
 @Author:    David Moeller Sztajnbok
-@Date:      October, 2023
+@Date:      November, 2023
 """
 
+import subprocess
 import os
-import sys
-import subprocess as sp
+
+avlGeom = 'test_geom.avl'
+avlCase = 'test_case.case'
+
+# Function to create the AVL command file
+def steadyLevelRoutine(geom, case, stOut='stOut.txt', filename='commands.run'):
+    routine = f"""\
+LOAD {geom}
+CASE {case}
+OPER
+C1
+
+x
+st
+{stOut}
+
+quit
+"""
+    with open(filename, 'w') as file:
+        file.write(routine)
 
 
-def saveGeometry(geom, loud=True):
-    pavl = sp.Popen(['AVL.exe'], stdin=sp.PIPE, stdout=None if loud else sp.DEVNULL, stderr=None)
+def runAvl(routine='commands.run', avlPath='avl'):
+    avl_command = f"{avlPath} < {routine}"
+    subprocess.run(avl_command, shell=True, stderr=subprocess.STDOUT)
 
-    def command(command):
-        if (sys.version_info > (3, 0)):
-            command = command.encode('ascii')
-        pavl.stdin.write(command)
+def parseOutput(stOut='stOut.txt'):
+    with open(stOut, 'r') as file:
+        lines = file.readlines()
 
-    command(f'load {geom}\n')
-    command('oper\n')
-    command('g\n')
-    command('h')
+    # Initialize variables
+    alpha = None
+    CLtot = None
+    CDtot = None
+    e = None
+    Elevator = None
+    Flaps = None
+    Ailerons = None
+    Rudder = None
+
+    # Iterate through each line and check for the required values
+    for line in lines:
+        try:
+            if 'Alpha' in line:
+                alpha = float(line.split('=')[1].split()[0])
+            elif 'CLtot' in line:
+                CLtot = float(line.split('=')[1].strip().split()[0])
+            elif 'CDtot' in line:
+                CDtot = float(line.split('=')[1].strip().split()[0])
+            elif 'e =' in line and 'Plane' in line:
+                e = float(line.split('=')[2].strip().split()[0])
+            elif 'Elevator' in line:
+                parts = line.split('=')
+                if len(parts) > 1:
+                    Elevator = float(parts[1].strip().split()[0])
+            elif 'Flaps' in line:
+                parts = line.split('=')
+                if len(parts) > 1:
+                    Flaps = float(parts[1].strip().split()[0])
+            elif 'Ailerons' in line:
+                parts = line.split('=')
+                if len(parts) > 1:
+                    Ailerons = float(parts[1].strip().split()[0])
+            elif 'Rudder' in line:
+                parts = line.split('=')
+                if len(parts) > 1:
+                    Rudder = float(parts[1].strip().split()[0])
+        except ValueError as ve:
+            print(f"ValueError: {ve} on line: {line}")
+        except IndexError as ie:
+            print(f"IndexError: {ie} on line: {line}")
+
+    return alpha, CLtot, CDtot, e, Elevator, Flaps, Ailerons, Rudder
+
+steadyLevelRoutine(avlGeom, avlCase)
+
+# Run AVL
+runAvl()
+
+# Parse the stability derivatives output
+alpha, CLtot, CDtot, e, Elevator, Flaps, Ailerons, Rudder = parseOutput()
+
+# Output the results
+print(f"Alpha: {alpha}")
+print(f"CLtot: {CLtot}")
+print(f"CDtot: {CDtot}")
+print(f"Oswald Efficiency (e): {e}")
+print(f"Elevator: {Elevator}") if Elevator else None
+print(f"Flaps: {Flaps}") if Flaps else None
+print(f"Ailerons: {Ailerons}") if Ailerons else None
+print(f"Rudder: {Rudder}") if Rudder else None
 
 
-def getStabilityDerivatives(geom, case, output, loud=True):
-    # pavl = sp.Popen(['avl.exe'], stdin=sp.PIPE, stdout=None if loud else sp.DEVNULL, stderr=None, universal_newlines=True)
-    pavl = sp.Popen(['avl.exe', f'load {geom}\n', f'case {case}\n', f'oper\nc1\n\nx\nst\n{output}.txt\n\nquit\n'], stdin=sp.PIPE, stdout=None if loud else sp.DEVNULL, stderr=None, universal_newlines=True)
-
-    def command(command):
-        pavl.stdin.write(command)
-
-    # # Load geometry and case
-    # command(f'load {geom}\n')
-    # command(f'case {case}\n')
-    #
-    # # Oper routine and set straight level trim, execute flow
-    # command('oper\n')
-    # command('g\n\n')
-    # command('c1\n\n')
-    # command('x\n')
-    #
-    # # Get stability derivatives and output
-    # command(f'st\n{output}.txt\n\n')
-    # command('quit\n')
+# Delete the stability derivatives output file
+os.remove('stOut.txt')
+os.remove('commands.run')
